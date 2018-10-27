2888a0368cda4ff0aaf8bc12f954c2ce7e38baa8
chiayi <chiayiliang327@gmail.com>
2018-10-26 21:16:43 -0400

    Modified User.py

--- User.py
+++ User.py
@@ -1,10 +1,9 @@
-from flask import Flask
 from flask import Flask, flash, request, redirect, render_template, session, abort
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('./home.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
 def login():
