9bf2314d5405625b2b19ed76a78fe2e8824d63c9
chiayi <chiayiliang327@gmail.com>
2018-10-27 01:00:06 -0400

    Modified User.py

--- User.py
+++ User.py
@@ -1,4 +1,11 @@
 from flask import Flask, flash, request, redirect, render_template, session, abort
+import sqlite3
+#from sqlalchemy import create_engine
+#from sqlalchemy import sql
+
+conn = sqlite3.connect("pathtofilehere")
+c = conn.cursor
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,10 +14,26 @@ def hello_world():
 
 @app.route('/login', methods=['POST'])
 def login():
-	if request.form['password'] == 'password' and request.form['username'] == 'admin':
-		session['logged_in'] = True
+	username = request.form['username']
+	password = request.form['password']
+	print(username)
+	print(password)
+	user = checkUser(username, password)
+	if user is not None :
+		return render_template('index.html')
 	else:
-		flash('wrong password!')
-	return 'Done'
+		error = "wrong password or username"
+		print(error)
+	return render_template('index.html', error=error)
+
+def checkUser(username, password):
+	rs = c.execute("SELECT * FROM username_tbl WHERE username = %s AND password = %s", (username, password))
+	# rs = con.execute("SELECT * FROM beers")
+	result = rs.first()
+	if result is None:
+		return None
+	else:
+		print(result)
+		return result 
 
 	
